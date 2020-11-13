eeb2bbe42769edb52ef3afdc497133f66206bbe7
Belinda Zou Li <26785408+belindal@users.noreply.github.com>
2020-11-13 14:43:00 -0500

    Save states

--- tw-play.py
+++ tw-play.py
@@ -10,6 +10,12 @@ import itertools
 import textworld
 import textworld.agents
 
+from textworld.render.graph import build_graph_from_facts, show_graph
+from textworld.logic import Proposition
+
+import logging
+import json
+import sys
 
 def build_parser():
     description = "Play a TextWorld game (.z8 or .ulx)."
@@ -23,6 +29,10 @@ def build_parser():
                         help="Limit maximum number of steps.")
     parser.add_argument("--seed", type=int, default=1234,
                         help="Seed for random and random-cmd agents.")
+    parser.add_argument("--state_fn", type=str, default=None,
+                        help="Filename for where to store state information for each command.")
+    parser.add_argument("--utts_fn", type=str, default=None,
+                        help="Filename for where to store utterances (language output).")
     parser.add_argument("--viewer", metavar="PORT", type=int, nargs="?", const=6070,
                         help="Start web viewer.")
     parser.add_argument("-v", "--verbose", action="store_true",
@@ -36,8 +46,29 @@ def main():
     args = build_parser().parse_args()
     if args.very_verbose:
         args.verbose = args.very_verbose
+    
+    if args.state_fn:
+        state_wf = logging.StreamHandler(open(args.state_fn, "w"))
+        state_wf.setLevel(logging.INFO)
+        state_logger = logging.getLogger("states")
+        state_logger.setLevel(logging.INFO)
+        state_logger.addHandler(state_wf)
+        # open(args.state_fn, 'w')
+    if args.utts_fn:
+        # bc = logging.basicConfig(filename=args.utts_fn)
+        utts_wf = logging.StreamHandler(open(args.utts_fn, "w"))
+        # utts_wf = open(args.utts_fn, 'w')
+    else:
+        # bc = logging.basicConfig()
+        utts_wf = logging.StreamHandler(sys.stdout)
+    utts_wf.terminator = ''
+    utts_wf.setLevel(logging.INFO)
+    utts_logger = logging.getLogger("utterances")
+    utts_logger.setLevel(logging.INFO)
+    utts_logger.addHandler(utts_wf)
 
-    env = textworld.start(args.game)
+    request_infos = textworld.EnvInfos(inventory=True, description=True, facts=True)
+    env = textworld.start(args.game, infos=request_infos)
 
     if args.mode == "random":
         agent = textworld.agents.NaiveAgent(seed=args.seed)
@@ -55,32 +86,46 @@ def main():
         env = HtmlViewer(env, port=args.viewer)
 
     if args.mode == "human" or args.very_verbose:
-        print("Using {}.\n".format(env.__class__.__name__))
+        utts_logger.info("Using {}.\n".format(env.__class__.__name__))
 
     game_state = env.reset()
     if args.mode == "human" or args.verbose:
         contents = env.render(mode="text")
         contents = '\n'.join(contents.split('\n')[22:])  # remove `TEXTWORLD` heading
         contents = contents.replace('\n\n', '\n')
-        print(contents, end="")
+        utts_logger.info(contents)
+        if args.state_fn:
+            state_logger.info(json.dumps({
+                'facts': [Proposition.serialize(prop) for prop in env.state.facts],
+                'inventory': env.state.inventory,
+                'description': env.state.description,
+            }))
 
     reward = 0
     done = False
 
     for _ in range(args.max_steps) if args.max_steps > 0 else itertools.count():
+        # tw_inform7.py
         command = agent.act(game_state, reward, done)
+        # env.state.game.world.facts
         game_state, reward, done = env.step(command)
 
         if args.mode == "human" or args.verbose:
             contents = env.render(mode="text")
             contents = contents.replace('\n\n', '\n').replace('\n\n', '\n')
-            print(contents, end="")
+            utts_logger.info(contents)
+            if args.state_fn:
+                state_logger.info(json.dumps({
+                    'facts': [Proposition.serialize(prop) for prop in env.state.facts],
+                    'inventory': env.state.inventory,
+                    'description': env.state.description,
+                }))
 
         if done:
             break
 
     env.close()
-    print("Done after {} steps. Score {}/{}.".format(game_state.moves, game_state.score, game_state.max_score))
+    utts_logger.info("Done after {} steps. Score {}/{}.".format(game_state.moves, game_state.score, game_state.max_score))
 
 
 if __name__ == "__main__":
